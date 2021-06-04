802f71c6eed9a0483631c637b55829ee2636ba44
jeongjuryu <totw2018@naver.com>
2021-06-04 12:48:59 +0900

    게시글 추가 기능

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const { Post, User } = require("../models");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.json({ id: 1, content: "hi" });
+router.post("/", async (req, res) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+    const fullPost = await Post.findOne({
+      where: { id: post.id },
+      include: [
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+          ],
+        },
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: User,
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 router.delete("/", (req, res) => {});
