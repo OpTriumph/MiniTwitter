114e8595e51d190d15e6eebc414065e401807c81
username <osthus@korea.ac.kr>
2021-06-13 15:09:33 +0900

    add redux-saga for login, signup

---
+++ front/pages/signup.js
@@ -0,0 +1,116 @@
+import React, { useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Router from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { signUpAction } from "../redux/user";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  submit: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+  const [email, setEmail] = React.useState("");
+  const [nickname, setNickname] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [check, setCheck] = React.useState("");
+  const dispatch = useDispatch();
+  const signUpError = useSelector((state) => state.userReducer.signUpError);
+  const signUpDone = useSelector((state) => state.userReducer.signUpDone);
+
+  useEffect(() => {
+    if (signUpError) {
+      alert(signUpError.data);
+    }
+    if (signUpDone) Router.push("/");
+  }, [signUpError, signUpDone]);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <TwitterIcon fontSize="large" color="primary" />
+        <Typography variant="h5">계정을 생성하세요</Typography>
+
+        <form>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="email"
+            name="email"
+            type="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="nickname"
+            label="nickname"
+            name="nickname"
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            error={password === check ? false : true}
+            helperText={password === check ? "" : "Different password"}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password-check"
+            label="password check"
+            type="password"
+            id="password-check"
+            value={check}
+            onChange={(e) => setCheck(e.target.value)}
+          />
+
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => {
+              dispatch(signUpAction({ email, password, nickname }));
+            }}
+          >
+            가입
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
