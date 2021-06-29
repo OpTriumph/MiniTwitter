e01838f41f9171eac20293cbe1735d49c826e938
JJeongRyu <39542775+JeongJuRyu@users.noreply.github.com>
2021-06-29 11:04:54 +0900

    Update app.js

--- back/app.js
+++ back/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   session({
-    secret: "hi",
+    secret: "secret",
     saveUninitialized: false,
     resave: false,
   })
